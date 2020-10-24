96473b8cf98faafdd286d4a1a09eb36f7ab062bb
Leo Battle <leowbattle@gmail.com>
2020-10-24 14:52:52 +0100

    Added naive rectangle packing algorithm.

--- RectanglePacker/DynamicRectanglePacker.cs
+++ RectanglePacker/DynamicRectanglePacker.cs
@@ -1,11 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace RectanglePacker
 {
 	public class DynamicRectanglePacker
 	{
+		List<Rectangle> freeRectangles;
+		List<Rectangle> allocatedRectangles;
 
+		public Size Size { get; }
+
+		public IEnumerable<Rectangle> FreeRectangles
+		{
+			get
+			{
+				return freeRectangles;
+			}
+		}
+
+		public IEnumerable<Rectangle> AllocatedRectangles
+		{
+			get
+			{
+				return allocatedRectangles;
+			}
+		}
+
+		public int FreeArea
+		{
+			get
+			{
+				int total = 0;
+				foreach (var rect in FreeRectangles)
+				{
+					total += rect.Width * rect.Height;
+				}
+				return total;
+			}
+		}
+
+		public int AllocatedArea
+		{
+			get
+			{
+				int total = 0;
+				foreach (var rect in AllocatedRectangles)
+				{
+					total += rect.Width * rect.Height;
+				}
+				return total;
+			}
+		}
+
+		public DynamicRectanglePacker(Size size)
+		{
+			Size = size;
+
+			Clear();
+		}
+
+		public Rectangle? Add(Size size)
+		{
+			// Find a free rectangle that is large enough to contain a rectangle with the specified size.
+			int index = freeRectangles.FindIndex(rect => rect.Width >= size.Width && rect.Height >= size.Height);
+
+			// If no rectangle large enough is found, return null.
+			if (index == -1)
+			{
+				return null;
+			}
+
+			// Remove the rectangle to be split from the list of free rectangles.
+			Rectangle rect = freeRectangles[index];
+			freeRectangles.RemoveAt(index);
+
+
+			// If the width taken up by the new rectangle is less than the width of the free rectangle, re-add the remaining space.
+			int xRemainder = rect.Width - size.Width;
+			if (xRemainder > 0)
+			{
+				freeRectangles.Add(new Rectangle(rect.X + size.Width, rect.Y, xRemainder, size.Height));
+			}
+
+			// Do the same, but with the space left over on the horizontal axis.
+			int yRemainder = rect.Height - size.Height;
+			if (yRemainder > 0)
+			{
+				freeRectangles.Add(new Rectangle(rect.X, rect.Y + size.Height, rect.Width, yRemainder));
+			}
+
+			freeRectangles.Sort((r1, r2) => r1.Height - r2.Height);
+
+			Rectangle result = new Rectangle(rect.Location, size);
+			allocatedRectangles.Add(result);
+
+			return result;
+		}
+
+		public void Clear()
+		{
+			freeRectangles = new List<Rectangle>
+			{
+				new Rectangle(Point.Empty, Size)
+			};
+			allocatedRectangles = new List<Rectangle>();
+		}
 	}
 }
--- RectanglePacker/Program.cs
+++ RectanglePacker/Program.cs
@@ -1,12 +1,91 @@
-﻿using System;
+﻿using SDL2;
+using System;
+using System.Drawing;
+using System.Linq;
 
 namespace RectanglePacker
 {
 	class Program
 	{
+		public IntPtr Window { get; private set; }
+		public IntPtr Renderer { get; private set; }
+
+		Size WindowSize = new Size(1920, 1080);
+
+		DynamicRectanglePacker packer = new DynamicRectanglePacker(new Size(1024, 1024));
+
+		Random random = new Random();
+
+		void Run()
+		{
+			SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+			Window = SDL.SDL_CreateWindow("Rectangle Packer", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, WindowSize.Width, WindowSize.Height, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+			Renderer = SDL.SDL_CreateRenderer(Window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+			while (true)
+			{
+				while (SDL.SDL_PollEvent(out var e) != 0)
+				{
+					switch (e.type)
+					{
+						case SDL.SDL_EventType.SDL_QUIT:
+							goto end;
+						case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+							packer.Clear();
+							for (int i = 0; i < 1000; i++)
+							{
+								var w = random.Next(10, 50);
+								var h = random.Next(10, 50);
+								packer.Add(new Size(w, h));
+							}
+							Console.WriteLine($"{packer.AllocatedRectangles.Count()}, wasted space = {packer.AllocatedArea / (float)(packer.Size.Width * packer.Size.Height) * 100}%");
+
+							break;
+						default:
+							break;
+					}
+				}
+
+				SDL.SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 0);
+				SDL.SDL_RenderClear(Renderer);
+
+				var rect = new Rectangle(Point.Empty, packer.Size);
+				DrawRect(rect, Color.Green);
+
+				foreach (var allocatedRect in packer.AllocatedRectangles)
+				{
+					allocatedRect.Inflate(-1, -1);
+					DrawRect(allocatedRect, Color.Red);
+				}
+
+				foreach (var freeRect in packer.FreeRectangles)
+				{
+					freeRect.Inflate(-1, -1);
+					DrawRect(freeRect, Color.Blue);
+				}
+
+				SDL.SDL_RenderPresent(Renderer);
+			}
+			end:;
+		}
+
+		void DrawRect(Rectangle rect, Color color)
+		{
+			SDL.SDL_SetRenderDrawColor(Renderer, color.R, color.G, color.B, color.A);
+
+			var sdlRect = new SDL.SDL_Rect
+			{
+				x = rect.X,
+				y = rect.Y,
+				w = rect.Width,
+				h = rect.Height
+			};
+			SDL.SDL_RenderFillRect(Renderer, ref sdlRect);
+		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			new Program().Run();
 		}
 	}
 }
